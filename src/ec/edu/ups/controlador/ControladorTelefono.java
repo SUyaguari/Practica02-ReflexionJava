30b5f1ab929816eeeb6a5991a9530e3ed49641dd
Sebastian Uyaguari <65028259+SUyaguari@users.noreply.github.com>
2020-11-12 20:21:39 -0500

    Clase controlador telefono

---
+++ src/ec/edu/ups/controlador/ControladorTelefono.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.ups.controlador;
+
+import ec.edu.ups.modelo.Telefono;
+import java.util.List;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class ControladorTelefono extends AbstractControlador<Telefono>{
+
+    @Override
+    public boolean validar(Telefono objeto) {
+        return true;
+    }
+
+    @Override
+    public void ordenarLista() {
+        List<Telefono> lista = getLista();
+        for (int i = 0; i < lista.size()-1; i++) {
+            for (int j = i+1; j < lista.size(); j++) {
+                if(lista.get(i).getNumero().compareTo(lista.get(j).getNumero())>0){
+                    var aux = lista.get(i);
+                    lista.set(i, lista.get(j));
+                    lista.set(j, aux);
+                }
+            }
+        }
+        setLista(lista);
+    }
+
+    @Override
+    public int generarId() {
+        List<Telefono> lista = getLista();
+        int codigo = 0;
+        if (lista.size()>0){
+            for (Telefono telefono : lista) {
+                int aux = telefono.getCodigo();
+                if(aux>codigo){
+                    codigo=aux;
+                }
+            }
+            return codigo+1;
+        }else{
+            return 1;
+            
+        }
+    }
+    
+}
